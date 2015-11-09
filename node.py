10946417816cf46798a4648fd1e0a80a8540393e
Eugene Viktorov <eugene.viktorov@anvil8.com>
2015-11-09 14:10:54 +0200

    add: full set of Unit tests for node.py

--- node.py
+++ node.py
@@ -2,7 +2,6 @@ import json
 import os.path
 import unittest
 
-from pathlib import Path
 from unittest.mock import patch, MagicMock, mock_open
 
 from database import files
@@ -146,13 +145,30 @@ class Node(object):
 
 
 class NodeTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Setup fake database values by patching "files" in imported "database"
+        module. Namely frame-up "files.selection().execute()" call results.
+        """
+        files.select = MagicMock()
+        files_size = []
+        select_all_files = [{'size': item} for item in files_size]
+        mock_execute = files.select.return_value
+        mock_execute.execute.return_value = select_all_files
+
     def setUp(self):
         self.node = Node(os.path.join(os.path.dirname(__file__),
                                       'test_node.json'))
 
-        with open(self.node._Node__file_path, 'r') as config_file:
-            init_node_data = json.load(config_file)
+        with open(self.node._Node__file_path, 'r') as main_json:
+            init_node_data = json.load(main_json)
+            main_json.seek(0)
+            with open('test_node_setup.json', 'w') as copy_json:
+                copy_json.write(main_json.read())
 
+        self.node._Node__file_path = os.path.join(os.path.dirname(__file__),
+                                                  'test_node_setup.json')
         self.data_from_test_nodeJSON = {
             '_Node__public_key': init_node_data['public_key'],
             '_Node__limits': init_node_data['bandwidth']['limits'],
@@ -164,6 +180,8 @@ class NodeTest(unittest.TestCase):
 
     def tearDown(self):
         del self.node
+        if 'test_node_setup.json' in os.listdir():
+            os.remove('test_node_setup.json')
 
     def test_node_get_instance(self):
         """
@@ -204,44 +222,64 @@ class NodeTest(unittest.TestCase):
         self.assertIsInstance(self.node._Node__current_bandwidth, dict)
 
     def test_node_capacity(self):
+        """
+        Test of correctness of working the capacity property
+        """
         self.assertIs(self.node._Node__capacity, self.node.capacity)
         self.assertIsInstance(self.node.capacity, int)
 
     def test_node_limits(self):
+        """
+        Test of correctness of working the limits property
+        """
         self.assertIs(self.node._Node__limits, self.node.limits)
         self.assertIsInstance(self.node.limits, dict)
 
     def test_node_total(self):
+        """
+        Test of correctness of working the node_total property
+        """
         self.assertIs(self.node._Node__total_bandwidth, self.node.total)
         self.assertIsInstance(self.node.total, dict)
 
     def test_node_public_key(self):
+        """
+        Test of correctness of working the public_key property
+        """
         self.assertIs(self.node._Node__public_key, self.node.public_key)
         self.assertIsInstance(self.node.public_key, str)
 
     def test_node_set_limits(self):
+        """
+        Test of setting up limits to node instance
+        """
         self.node.set_limits(incoming=200, outgoing=300)
         self.assertEqual(
             (
                 self.node._Node__limits['incoming'],
                 self.node._Node__limits['outgoing'],
             ),
-            (
-                200,
-                300,
-            )
+            (200, 300,)
         )
 
     def test_node_store(self):
         """
-        Test rewrite node's json
+        Test of ability to rewrite node's json
         """
         mocked_open = mock_open()
         with patch('node.open', mocked_open, create=True):
             self.node._store()
         mocked_open.assert_called_once_with(self.node._Node__file_path, 'w')
-
-
+        with open(self.node._Node__file_path, 'w') as file:
+            file.write('corrupting the settings file for further repairing')
+        self.node._store()
+        self.assertDictEqual(
+            json.load(open(self.node._Node__file_path)),
+            json.load(open(
+                os.path.join(os.path.dirname(__file__), 'test_node.json')
+            )),
+            "problems with rewriting a json file of the Node "
+        )
 
     @patch.object(files, 'select')
     def test_node_info(self, mock_select):
@@ -259,8 +297,24 @@ class NodeTest(unittest.TestCase):
             init_node_data['storage']['max_file_size'] = max(files_size)
             init_node_data['storage']['used'] = sum(files_size)
 
-        self.assertEqual(init_node_data, self.node.info)
+        self.assertDictEqual(init_node_data, self.node.info)
 
+    def test_node_increase_traffic(self):
+        """
+        Test for Node.__increase_traffic()
+        """
+        info_before = self.node.info
+        self.node._Node__increase_traffic(100)
+        self.node._Node__increase_traffic(300, False)
+        info_before['bandwidth'].update(
+            total={'incoming': 100, 'outgoing': 300},
+            current={'incoming': 100, 'outgoing': 300}
+        )
+        self.assertDictEqual(
+            self.node.info,
+            info_before,
+            "Node.__increase_traffic() doesn't updating data of Node instance"
+        )
 
 
 if __name__ == "__main__":
