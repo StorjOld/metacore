c13396a965f883d53c8abc7bf6bffd2bf0cd42cd
Eugene Viktorov <eugene.viktorov@anvil8.com>
2015-11-26 19:50:07 +0200

    chages: add setup.py and move metacore to the metacore module directory

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    include_package_data = True,
+    name='metacore',
+    version='1.0',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'Flask>=0.10,<0.11',
+        'SQLAlchemy>=1.0',
+        'btctxstore',
+        'file_encryptor',
+        'uwsgi',
+    ],
+    test_suite='metacore.tests',
+    entry_points={
+        'console_scripts':
+            ['metacore = metacore.storj:main']
+    }
+)
