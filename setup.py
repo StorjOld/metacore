fae358445b34e26bf4f7b393bd6185ff3e32a15f
Eugene Viktorov <eugene.viktorov@anvil8.com>
2015-12-02 15:59:50 +0200

    add: separate required package set for python 2x and 3x

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
+import sys
 from setuptools import setup, find_packages
 
+
+required_packages = ['Flask>=0.10,<0.11', 'SQLAlchemy>=1.0',
+                     'btctxstore', 'file_encryptor']
+if sys.version_info.major == 2:
+    required_packages.insert(0, 'mock')
+
+
 setup(
-    include_package_data = True,
+    include_package_data=True,
     name='metacore',
     version='1.0',
-
     packages=find_packages(),
-
-    install_requires=[
-        'mock',
-        'Flask>=0.10,<0.11',
-        'SQLAlchemy>=1.0',
-        'btctxstore',
-        'file_encryptor',
-    ],
+    install_requires=required_packages,
     test_suite='metacore.tests',
     entry_points={
         'console_scripts':
