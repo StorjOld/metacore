5745d35e78d03859ef3c085d25d706847fecc5ed
Eugene Viktorov <eugene.viktorov@anvil8.com>
2015-12-18 12:21:14 +0200

    add: file __main__.py like the entry point to the module

---
+++ metacore/__main__.py
@@ -0,0 +1,16 @@
+import os.path
+import sys
+
+parentdir = os.path.dirname(os.path.dirname(__file__))
+
+if not parentdir in sys.path:
+    sys.path.insert(0, parentdir)
+
+from metacore.storj import app as application
+
+
+def main():
+    application.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    main()
